272cb5e388ec27d09ec99f71d06299419e51dfc4
avsky0095 <andrianovsky95@gmail.com>
2022-06-04 14:45:20 +0700

    update terraform version

--- providers.tf
+++ providers.tf
@@ -1,12 +1,12 @@
 
-# terraform {
-#   required_providers {
-#     aws = {
-#       source = "hashicorp/aws"
-#       version = "4.14.0"
-#     }
-#   }
-# }
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "4.16.0"
+    }
+  }
+}
 
 provider "aws" {
   region     = var.region
